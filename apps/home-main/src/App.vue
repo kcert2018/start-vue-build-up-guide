1fc87c225131095d00d4eadb49b53e54a1cabeb7
frog <frog@falinux.com>
2019-01-19 11:15:17 +0900

    backup

--- apps/home-main/src/App.vue
+++ apps/home-main/src/App.vue
@@ -1,31 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>First!</h1>
   </div>
 </template>
 
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+    }),
+
+    sample: {
+      get () { return '' },
+      set (newValue) {}
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    ...mapMutations({
+    }),
+    ...mapActions({
+    })
+  },
+
+  created () { console.log('CALL created()') },
+  mounted () { console.log('CALL mounted()') },
+  activated () { console.log('CALL activated()') },
+  deactivated () { console.log('CALL deactivated()') },
+  destroyed () { console.log('CALL destroyed()') }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+</style>
+<style scoped>
 </style>
