e6a34b9e999aee62f8591ef50149d26ce7931a04
frog <frog@falinux.com>
2019-01-19 14:18:55 +0900

    backup

---
+++ apps/home-main/src/views/messages-main.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="messages-main">
+    <h1>Home Main</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'messages-main',
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+    }),
+
+    sample: {
+      get () { return '' },
+      set (newValue) {}
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    ...mapMutations({
+    }),
+    ...mapActions({
+    })
+  },
+
+  created () { console.log('CALL created()') },
+  mounted () { console.log('CALL mounted()') },
+  activated () { console.log('CALL activated()') },
+  deactivated () { console.log('CALL deactivated()') },
+  destroyed () { console.log('CALL destroyed()') }
+}
+</script>
+
+<style>
+</style>
+<style scoped>
+</style>
