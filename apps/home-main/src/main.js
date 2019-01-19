531eed9096270a8585c27f0304a7e5eae84fce74
frog <frog@falinux.com>
2019-01-19 10:41:22 +0900

    backup

--- apps/home-main/src/main.js
+++ apps/home-main/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { createProvider } from './vue-apollo'
+// import { createProvider } from './vue-apollo'
 
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
@@ -15,6 +15,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  apolloProvider: createProvider(),
+  // apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
