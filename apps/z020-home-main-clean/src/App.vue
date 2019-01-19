cfd5800a69560ce8510dd7bf808aa3413ebaebac
frog <frog@falinux.com>
2019-01-19 12:05:15 +0900

    backup

---
+++ apps/z020-home-main-clean/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1>First!</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+    }),
+
+    sample: {
+      get () { return '' },
+      set (newValue) {}
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    ...mapMutations({
+    }),
+    ...mapActions({
+    })
+  },
+
+  created () { console.log('CALL created()') },
+  mounted () { console.log('CALL mounted()') },
+  activated () { console.log('CALL activated()') },
+  deactivated () { console.log('CALL deactivated()') },
+  destroyed () { console.log('CALL destroyed()') }
+}
+</script>
+
+<style>
+</style>
+<style scoped>
+</style>
